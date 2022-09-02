01d0c1f78893b347118665a383b4fb0458d09da6
Vu Nghia <Vu Nghia@DESKTOP-P2BL7QB>
2022-09-02 16:02:13 +0700

    Add project files.

---
+++ VuTrungNghia/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace VuTrungNghia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Q = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Q
+            // 
+            this.Q.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Q.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.Q.Location = new System.Drawing.Point(146, 83);
+            this.Q.Name = "Q";
+            this.Q.Size = new System.Drawing.Size(94, 100);
+            this.Q.TabIndex = 0;
+            this.Q.Text = "Sign In";
+            this.Q.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.Q.UseVisualStyleBackColor = false;
+            this.Q.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(438, 170);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(300, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Q);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Q;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ VuTrungNghia/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace VuTrungNghia
+{
+    public partial class Form1 : Form
+    {
+        private const string V = "hoa khung";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            Q.Text = ("xin chao 20DTHA5");
+            this.BackColor = Color.Yellow;
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            button1.Text = "xin chao 20DTHA5" + textBox1.Text;
+        }
+        private void textbox1_textchanged(object sender, eventargs e)
+
+        {
+            
+        }
+        
+       
+    }
+}
